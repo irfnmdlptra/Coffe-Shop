53b3b53e0f9a211d328e491974a55f7bb77af40a
Irfnmdlptra <bangasrat@gmail.com>
2023-01-30 18:37:38 +0800

    footer

--- form/contact.js
+++ form/contact.js
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+const scriptURL = "https://script.google.com/macros/s/AKfycbynJK-3ZgIY82-DsiMoGvtUrCoL6Q3wBn14NOCChQQSRzsrq_nI4CyTojV5-NhV8_b2/exec";
+const form = document.forms["cofshop-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+//   btnKirim.addEventListener('click', function(){
+//     alert('cobabab')
+//   })
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Menghilakan Tombol Kirim
+  btnLoading.classList.toggle("none");
+  btnKirim.classList.toggle("none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      myAlert.classList.toggle("none");
+      btnLoading.classList.toggle("none");
+      btnKirim.classList.toggle("none");
+      window.addEventListener('click', function(){
+        myAlert.classList.add('none')
+      })
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
